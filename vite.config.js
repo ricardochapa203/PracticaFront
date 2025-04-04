2cfa5cee6d675e309fc52a0f64971bb11f537eea
ricardochapa203 <a01722755@tec.mx>
2025-04-03 20:20:11 -0600

    Corregido

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PracticaFront/', // Asegúrate de usar el nombre del repositorio aquí
 })
